fff5923ec5ed4b55da68e6c0f1c433bdb3235871
NikhilD <ndeshmukh7030@gmail.com>
2025-09-28 17:08:19 +0530

    adding new programm

---
+++ nonRepeatingCharacter.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class main{
+
+    public static void main(String [] args){
+
+        String str = "programming";
+
+        Map<Character, Integer> map = new LinkedHashMap<>();
+
+        for(char c :str.toCharArray())
+        map.put(c , map.getOrDefault(c ,0) +1);
+        System.out.println(map);
+        for(var e :map.entrySet())
+            if(e.getValue() == 1){
+                System.out.println(e.getKey());
+                break;
+                
+            }
+
+    }
+}
